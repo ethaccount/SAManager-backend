4138cd271213ece71e27965375972942f4b10423
Johnson Chen <chnejohnson@gmail.com>
2025-06-04 16:22:01 +0800

    chore: add license

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,8 +23,7 @@ import (
 // @contact.url    http://www.swagger.io/support
 // @contact.email  support@swagger.io
 
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @license.name  AGPL-3.0-only
 
 // @host      localhost:8080
 // @BasePath  /api/v1
